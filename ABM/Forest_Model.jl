0fe57201c13f082415188aaa8409eacf97a40b43
csim063 <simpkinscraig063@gmail.com>
2022-09-21 15:06:18 +1200

    Visualisations added

--- ABM/Forest_Model.jl
+++ ABM/Forest_Model.jl
@@ -24,22 +24,22 @@ include("Modules/Step.jl")
 
 #//---------------------------------------------------------------------------#
 #% Import data from files
-demography_df = DataFrame(CSV.File("Data/demography.txt"))
-site_df = DataFrame(CSV.File("Data/forest.txt"))
+demography_df = DataFrame(CSV.File("Data/demography.txt"));
+site_df = DataFrame(CSV.File("Data/forest.txt"));
 
 #//---------------------------------------------------------------------------#
 #% Setup model
 ## First setup the dataframe where data will be saved
-adata = [:pos, :species_ID, :growth_form, :height, :dbh, :age]
+adata = [:pos, :species_ID, :growth_form, :height, :dbh, :age];
 
-model = Setup.forest_model(forest_area = 4,
+model = Setup.forest_model(forest_area = 16,
                            cell_grain = 4, 
                            n_species = 8,
-                           edge_strength = 1.0,
+                           edge_strength = 0.0,
                            max_shade_distance = 32,
                            site_df = site_df,
                            demography_df = demography_df,
-                           disturb_freq = 0.200,
+                           disturb_freq = 0.100,
                            max_disturb_size = 0.40,
                            comp_multiplier = 1.60,
                            edge_effects = false,
@@ -51,31 +51,93 @@ model = Setup.forest_model(forest_area = 4,
                            ddm = false,
                            restoration_planting = false,
                            planting_frequency = 10
-                           )
+                           );
 
 #//---------------------------------------------------------------------------#
 #% Step model
 #step!(model, go.agent_step!, go.model_step!, 1)
-data, _ = run!(model, go.agent_step!, go.model_step!, 5; adata = adata, when = [5])
-data[1:100, :]
+data, _ = run!(model, go.agent_step!, go.model_step!, 50; adata = adata, when = [5]);
+run!(model, go.agent_step!, go.model_step!, 50);
+# data[1:100, :]
 
 # Quick and dirty performance benchmarking
-@time run!(model, go.agent_step!, go.model_step!, 5);
+#@time run!(model, go.agent_step!, go.model_step!, 5);
 
 #//---------------------------------------------------------------------------#
 #% Visualise
 cols = palette("Spectral", nrow(site_df));
-speciescolor(a) = a.species_ID == 0 ? :white : cols[a.species_ID]
+speciescolor(a) = a.species_ID == 0 ? :white : cols[a.species_ID];
 
-speciesshape(a) = a.growth_form == 1 ? :circle : :diamond
-speciessize(a) = a.age == 0 ? 8 : (8 + (a.age * 0.025))
+speciesshape(a) = a.growth_form == 1 ? :circle : :diamond;
+speciessize(a) = a.age == 0 ? 8 : (8 + (a.age * 0.025));
 
-fig, _  = abmplot(model;
+plotkwargs = (;
         ac = speciescolor,
         am = speciesshape,
-        as = speciessize
+        as = speciessize,
+        scatterkwargs = (strokewidth = 0.5,)
+);
+
+params = Dict(
+        :disturbance_freq => 0.1:0.05:1.0,
+        :max_disturb_size => 0.1:0.05:1.0,
+        :comp_multiplier => 0.0:0.05:3.0,
+        :edge_effects => 0:1:1,
+        :external_rain => 0:1:1,
+        :herbivory => 0:1:1,
+        :saplings_eaten => 0:1:1,
+        :macro_litter_effect => 0.0:0.01:1.0,
+        :ddm => 0:1:1,
+        :restoration_planting => 0:1:1,
+        :planting_frequency => 1:1:10
+);
+
+#* Static plot
+fig, ax, abmobs  = abmplot(model;
+        ac = speciescolor,
+        am = speciesshape,
+        as = speciessize,
+        scatterkwargs = (strokewidth = 0.5,)
 )
 fig
 
+#* Interactive plot
+using GLMakie
+
+fig, ax, abmobs = abmplot(model;
+                agent_step! = go.agent_step!,
+                model_step! = go.model_step!,
+                params,
+                plotkwargs...);
+fig
+
+#* Exploration plot
+using Statistics: mean
+Tawa(a) = a.species_ID == :1
+Pigeonwood(a) = a.species_ID == :2
+
+adata = [(Tawa, count), (Pigeonwood, count)]
+
+seedling_count(model) = sum(sum(model.seedlings))
+sapling_count(model) = sum(sum(model.saplings))
+
+mdata = [seedling_count, sapling_count]
+
+fig, abmobs = abmexploration(model;
+                agent_step! = go.agent_step!,
+                model_step! = go.model_step!,
+                params,
+                plotkwargs...,
+                adata, alabels =  ["Tawa", "Pigeonwood"],
+                mdata, mlabels = ["Seedlings", "Saplings"]);
+fig
+
+#* Video
+abmvideo(
+        "./Outputs/Videos/Development.mp4",
+        model, go.agent_step!, go.model_step!;
+        title ="Development", frames = 500,
+        plotkwargs...
+)
 #//---------------------------------------------------------------------------#
 #% Export data
